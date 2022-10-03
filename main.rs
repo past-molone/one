2f4fcafbd026b3af50dec026863821d62a9d9b7a
past-molone <kin_fantasia@hotmail.com>
2022-10-03 21:07:43 +0700

    first time with this shit language

---
+++ main.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Write};
+
+fn main() {
+
+    print!("Please enter a number 1 or 2: ");
+
+    io::stdout().flush().unwrap();
+
+    let mut a = String::new();
+
+    io::stdin()
+        .read_line(&mut a)
+        .expect("");
+
+    let a = a.trim();
+
+    println!("----------------------------------------------------------------");
+
+    if a.parse::<isize>() == Ok(1){
+        for number in 1..11{
+            println!("{number}");
+        }
+    }
+    else if a.parse::<isize>() == Ok(2){
+        for number in (1..11).rev(){
+            println!("{number}");
+        }
+    }
+}

b04d3685d74b4199b2d33841badf73d258b4d40e
past-molone <kin_fantasia@hotmail.com>
2022-10-06 19:29:56 +0700

    fufu

--- src/vecotr.rs
+++ src/vecotr.rs
@@ -1,16 +1,16 @@
 pub fn pv(){
-    let v1 = vec!(1;10);
+    let v1 = vec![1,2,3,4,5,6,7,8,9,10];
 
-    for members in v1{
-        if members != 2{
+    for members in &v1{
+        if members != &v1[8] && members != &v1[9]{
             print!("{:?}, ",members);
         }
+        else if members == &v1[8]{
+            print!("{members}");
+        }
         else {
-            print!("This is the last member: {members}");  
+            println!("");
+            print!("And the last member is : {members}");  
         }
     }
-
-    let x = 5;
-    let y = x;
-    print!("{x},{y}")
 }
